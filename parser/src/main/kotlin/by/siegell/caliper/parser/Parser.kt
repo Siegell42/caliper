cf97f828e0919ee527176dd3337135e1ec880fd0
Pavel Zinkevich <p.zinkevich42@gmail.com>
2025-01-19 00:29:54 +0300

    Отформатировал класс парсера перед расширением.

--- parser/src/main/kotlin/by/siegell/caliper/parser/Parser.kt
+++ parser/src/main/kotlin/by/siegell/caliper/parser/Parser.kt
@@ -4,37 +4,45 @@ import by.siegell.caliper.domain.CaliperClassInfo
 import by.siegell.caliper.domain.CaliperFunctionInfo
 import by.siegell.caliper.domain.CaliperParameterInfo
 import by.siegell.caliper.domain.CaliperPropertyInfo
-import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.symbol.*
 
 class Parser {
 
-    fun analyzeClass(ksClass: KSClassDeclaration): CaliperClassInfo {
-        return CaliperClassInfo(
-            name = ksClass.simpleName.asString(),
-            packageName = ksClass.packageName.asString(),
-            properties = ksClass.getAllProperties()
-                .map { property ->
-                    CaliperPropertyInfo(
-                        name = property.simpleName.asString(),
-                        type = property.type.resolve().declaration.qualifiedName?.asString().orEmpty()
-                    )
-                }
+    fun analyzeClass(ksClass: KSClassDeclaration) = with(ksClass) {
+        CaliperClassInfo(
+            name = simpleName.asString(),
+            packageName = packageName.asString(),
+            properties = getAllProperties()
+                .map { analyzeProperty(it) }
                 .toList(),
-            functions = ksClass.getAllFunctions()
-                .map { function ->
-                    CaliperFunctionInfo(
-                        name = function.simpleName.asString(),
-                        parameters = function.parameters.map { parameter ->
-                            CaliperParameterInfo(
-                                name = parameter.name?.asString().orEmpty(),
-                                type = parameter.type.resolve().declaration.qualifiedName?.asString().orEmpty()
-                            )
-                        },
-                        returnType = function.returnType?.resolve()?.declaration?.qualifiedName?.asString().orEmpty(),
-                        body = null
-                    )
-                }
+            functions = getAllFunctions()
+                .map { analyzeFunction(it) }
                 .toList()
         )
     }
+
+    private fun analyzeProperty(ksProperty: KSPropertyDeclaration) = with(ksProperty) {
+        CaliperPropertyInfo(
+            name = simpleName.asString(),
+            type = type.asString()
+        )
+    }
+
+    private fun analyzeFunction(ksFunction: KSFunctionDeclaration) = with(ksFunction) {
+        CaliperFunctionInfo(
+            name = simpleName.asString(),
+            parameters = parameters.map { analyzeParameter(it) },
+            returnType = returnType.asString(),
+            body = null
+        )
+    }
+
+    private fun analyzeParameter(ksValueParameter: KSValueParameter) = with(ksValueParameter) {
+        CaliperParameterInfo(
+            name = name?.asString().orEmpty(),
+            type = type.asString()
+        )
+    }
+
+    private fun KSTypeReference?.asString(): String = this?.resolve()?.declaration?.qualifiedName?.asString().orEmpty()
 }
